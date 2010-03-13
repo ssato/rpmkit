d679e3de5256f7fd364ef25502df66f5b77cf68e
Satoru SATOH <satoru.satoh@gmail.com>
2010-03-14 04:11:52 +0900

    removed hack to sort result

--- list-srpmnames-by-file.sh
+++ list-srpmnames-by-file.sh
@@ -19,11 +19,12 @@ fi
 
 filename0="${filename:0:1}"
 if test "x$filename0" = "x/"; then
-    sql="SELECT p.srpmname FROM packages as p, files as f WHERE p.pid = f.pid AND f.path = \"$filename\""
+    sql="SELECT DISTINCT p.srpmname FROM packages as p, files as f WHERE p.pid = f.pid AND f.path = \"$filename\""
 else
-    sql="SELECT p.srpmname FROM packages as p, files as f WHERE p.pid = f.pid AND f.basename = \"$filename\""
+    sql="SELECT DISTINCT p.srpmname FROM packages as p, files as f WHERE p.pid = f.pid AND f.basename = \"$filename\""
 fi
 
-$sqlite $sqldb "$sql" | sort | uniq
+#$sqlite $sqldb "$sql" | sort
+$sqlite $sqldb "$sql"
 
 # vim: set sw=4 ts=4 et:
