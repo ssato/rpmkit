aac81b7cd05c78496befb6b832d83db6667124d9
Satoru SATOH <satoru.satoh@gmail.com>
2012-02-23 11:28:56 +0900

    myrepo: added a global, REPO_DEFAULT, to save repository specific parameter defaults

--- rpmkit/myrepo/globals.py
+++ rpmkit/myrepo/globals.py
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2011 Red Hat, Inc.
+# Copyright (C) 2011, 2012 Red Hat, Inc.
 # Red Hat Author(s): Satoru SATOH <ssato@redhat.com>
 #
 # This program is free software: you can redistribute it and/or modify
@@ -15,11 +15,24 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-# import ...
-#
+import rpmkit.Bunch as B
+
+
 # timeouts [sec]:
 (REMOTE_TIMEOUT, BUILD_TIMEOUT, LOCAL_TIMEOUT, MIN_TIMEOUT) = (
     60 * 10, 60 * 10, 60 * 5, 5
 )
+
+REPO_DEFAULT = B.Bunch(
+    name="%(distname)s-%(hostname)s-%(user)s",
+    subdir="yum",
+    topdir="~%(user)s/public_html/%(subdir)s",
+    baseurl="http://%(server)s/%(user)s/%(subdir)s/%(distdir)s",
+    signkey="",
+    keydir="/etc/pki/rpm-gpg",
+    keyurl="file://%(keydir)s/RPM-GPG-KEY-%(name)s-%(distversion)s",
+    metadata_expire="2h",
+)
+
 
 # vim:sw=4 ts=4 et:
