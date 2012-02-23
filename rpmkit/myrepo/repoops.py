63673acbcd3a5b7d61da73445886f68dd4386808
Satoru SATOH <satoru.satoh@gmail.com>
2012-02-23 11:27:47 +0900

    myrepo: updated copyright headers

--- rpmkit/myrepo/repoops.py
+++ rpmkit/myrepo/repoops.py
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2011 Red Hat, Inc.
+# Copyright (C) 2011, 2012 Red Hat, Inc.
 # Red Hat Author(s): Satoru SATOH <ssato@redhat.com>
 #
 # This program is free software: you can redistribute it and/or modify
