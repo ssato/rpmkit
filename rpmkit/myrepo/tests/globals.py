1c76446f20e27f1e456ff11e075a1c520afcc002
Satoru SATOH <satoru.satoh@gmail.com>
2012-03-02 12:03:44 +0900

    myrepo: fixed a bug that importing wrong module

--- rpmkit/myrepo/tests/globals.py
+++ rpmkit/myrepo/tests/globals.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-import rpmkit.myrepo.environ as G
+import rpmkit.myrepo.globals as G
 
 
 # vim:sw=4 ts=4 et:
