239c17b8dfdca3f0dbed9dcb5256cb31b5ebceb1
Satoru SATOH <satoru.satoh@gmail.com>
2012-05-11 20:34:43 +0900

    removed unused imports

--- rpmkit/shell.py
+++ rpmkit/shell.py
@@ -15,7 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-import rpmkit.Bunch as B
 import rpmkit.environ as E
 import rpmkit.utils as U
 
