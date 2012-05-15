806a51130f24d952834e2f96e937599d29d868bc
Satoru SATOH <satoru.satoh@gmail.com>
2012-05-15 22:11:02 +0900

    fixed a bug that module import path was wrong

--- rpmkit/rpmdb/models/packages.py
+++ rpmkit/rpmdb/models/packages.py
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-from rpmkit.rpmdb.base import DeclBase, DeclMixin
+from rpmkit.rpmdb.models.base import DeclBase, DeclMixin
 
 import sqlalchemy as S
 import sqlalchemy.orm as SO
