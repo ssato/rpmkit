bdf04550303ae739332e65d000f3492868b64b9e
Satoru SATOH <satoru.satoh@gmail.com>
2012-05-17 20:21:11 +0900

    updated

--- rpmkit/rhncachedb.py
+++ rpmkit/rhncachedb.py
@@ -18,6 +18,11 @@
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 #
 import rpmkit.sqlminus as SQ
+import logging
+import optparse
+import os.path
+import sqlite3
+import sys
 
 
 # all_packages_in_channel in 
@@ -113,6 +118,82 @@ FROM
   ON ECVE.errata_id = CE.errata_id
 WHERE C.label = '%s' AND ECVE.cve_id = CVE.id
 """
+
+# spacewalk.git/schema/spacewalk/common/tables/rhnPackageName.sql
+# spacewalk.git/schema/spacewalk/common/tables/rhnPackageEVR.sql
+# spacewalk.git/schema/spacewalk/common/tables/rhnPackageArch.sql
+PACKAGES_CREATE = """
+CREATE TABLE packages IF NOT EXISTS (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(256) NOT NULL,
+    version VARCHAR(512) NOT NULL,
+    release VARCHAR(512) NOT NULL,
+    epoch VARCHAR(16),
+    arch VARCHAR(64) NOT NULL
+)
+"""
+PACKAGES_INS = "INSERT INTO packages VALUES (?, ?, ?, ?, ?, ?)"
+
+
+# spacewalk.git/schema/spacewalk/common/tables/rhnPackageFile.sql
+# spacewalk.git/schema/spacewalk/common/tables/rhnPackageCapability.sql
+PACKAGE_FILES_CREATE = """
+CREATE TABLE package_files IF NOT EXISTS (
+    package_id INTEGER CONSTRAINT pf_ps REFERENCES packages(id) ON DELETE CASCADE,
+    name VARCHAR(4000) NOT NULL
+)
+"""
+PACKAGE_FILES_INS = "INSERT INTO package_files VALUES (?, ?)"
+
+# spacewalk.git/schema/spacewalk/common/tables/rhnPackageRequires.sql
+# spacewalk.git/schema/spacewalk/common/tables/rhnPackageCapability.sql
+PACKAGE_REQUIRES_CREATE = """
+CREATE TABLE package_requires IF NOT EXISTS (
+    package_id INTEGER CONSTRAINT pr_ps REFERENCES packages(id) ON DELETE CASCADE,
+    name VARCHAR(4000) NOT NULL,
+    modifier VARCHAR(100) NOT NULL
+)
+"""
+PACKAGE_REQUIRES_INS = "INSERT INTO package_requires VALUES (?, ?, ?)"
+
+# spacewalk.git/schema/spacewalk/common/tables/rhnPackageProvides.sql
+# spacewalk.git/schema/spacewalk/common/tables/rhnPackageCapability.sql
+PACKAGE_PROVIDES_CREATE = """
+CREATE TABLE package_provides IF NOT EXISTS (
+    package_id INTEGER CONSTRAINT pp_ps REFERENCES packages(id) ON DELETE CASCADE,
+    name VARCHAR(4000) NOT NULL,
+    modifier VARCHAR(100) NOT NULL
+)
+"""
+PACKAGE_PROVIDES_INS = "INSERT INTO package_provides VALUES (?, ?, ?)"
+
+# spacewalk.git/schema/spacewalk/common/tables/rhnErrata.sql
+ERRATA_CREATE = """
+CREATE TABLE errata IF NOT EXISTS (
+    id INTEGER PRIMARY KEY,
+    advisory VARCHAR(100) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    synopsis VARCHAR(4000) NOT NULL,
+    issue_date VARCHAR(100) NOT NULL
+)
+"""
+ERRATA_INS = "INSERT INTO errata VALUES (?, ?, ?, ?, ?)"
+
+PACKAGE_ERRATA_CREATE = """
+CREATE TABLE package_errata IF NOT EXISTS (
+    package_id INTEGER CONSTRAINT pe_ps REFERENCES packages(id) ON DELETE CASCADE,
+    errata_id INTEGER CONSTRAINT pe2_ps REFERENCES errata(id) ON DELETE CASCADE
+)
+"""
+PACKAGE_ERRATA_INS = "INSERT INTO package_errata VALUES (?, ?)"
+
+ERRATA_CVES_CREATE = """
+CREATE TABLE errata_cves IF NOT EXISTS (
+    errata_id INTEGER CONSTRAINT ec_ps REFERENCES errata(id) ON DELETE CASCADE,
+    name VARCHAR(13)
+)
+"""
+ERRATA_CVES_INS = "INSERT INTO errata_cves VALUES (?, ?)"
 
 
 def ts2d(tuple, keys):
@@ -249,6 +330,100 @@ def get_errata_cves(conn, repo):
     keys = ("errata_id", "name")
 
     return [ts2d(r, keys) for r in rs]
+
+
+def create_db(output):
+    conn = sqlite3.connect(output)
+    cur = conn.cursor()
+
+    for ddl in (PACKAGES_CREATE, PACKAGE_FILES_CREATE, PACKAGE_REQUIRES_CREATE,
+            PACKAGE_PROVIDES_CREATE, ERRATA_CREATE, PACKAGE_ERRATA_CREATE,
+            ERRATA_CVES_CREATE):
+        cur.execute(ddl)
+
+    conn.commit()
+    cur.close()
+
+
+
+def collect_and_dump_data(dsn, repo, output):
+    iconn = SQ.connect(dsn)
+
+    oconn = sqlite3.connect(output)
+    cur = conn.cursor()
+
+    packages = get_packages(iconn, repo)
+    cur.executemany(PACKAGES_INS, packages)
+    oconn.commit()
+
+    errata = get_errata(iconn, repo)
+    cur.executemany(ERRATA_INS, errata)
+    oconn.commit()
+
+    package_files = get_packages_files(iconn, repo)
+    cur.executemany(PACKAGE_FILES_INS, package_files)
+    oconn.commit()
+
+    package_errata = get_packages_errata(iconn, repo)
+    cur.executemany(PACKAGE_ERRATA_INS, package_errata)
+    oconn.commit()
+
+    package_requires = get_packages_requires(iconn, repo)
+    cur.executemany(PACKAGE_REQUIRES_INS, package_requires)
+    oconn.commit()
+
+    package_provides = get_packages_provides(iconn, repo)
+    cur.executemany(PACKAGE_PROVIDES_INS, package_provides)
+    oconn.commit()
+
+    errata_cves = get_errata_cves(iconn, repo)
+    cur.executemany(ERRATA_CVES_INS, errata_cves)
+    oconn.commit()
+
+    cur.close()
+
+
+def option_parser(prog="swapi"):
+    defaults = dict(
+        output=None,
+        dsn="rhnsat/rhnsat@rhnsat",
+        debug=False,
+    )
+    p = optparse.OptionParser("%prog [OPTION ...] CHANNEL_LABEL")
+    p.set_defaults(**defaults)
+
+    p.add_option("-o", "--output", help="Output filename [<channel_label>.db]")
+    p.add_option("", "--dsn", help="Data source name [%default]")
+    p.add_option("-D", "--debug", action="store_true", help="Debug mode")
+
+    return p
+
+
+def main(argv=sys.argv):
+    logging.getLogger().setLevel(logging.INFO)
+
+    p = option_parser()
+    (options, args) = p.parse_args(argv[1:])
+
+    if not args:
+        p.print_help()
+        sys.exit(0)
+
+    if options.debug:
+        logging.getLogger().setLevel(logging.DEBUG)
+
+    chan = args[0]
+
+    if not options.output:
+        options.output = chan + ".db"
+
+    create_db(options.output)
+
+    collect_and_dump_data(options.dsn, chan, options.output)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
 
 
 # vim:sw=4:ts=4:et:
