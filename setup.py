84b8e4af5b1f71577cff9453360b1a5a6f7f0fe4
Satoru SATOH <satoru.satoh@gmail.com>
2012-06-21 17:32:13 +0900

    switched back to snaphsot versioning mode

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ PACKAGE = "rpmkit"
 VERSION = "0.2.10.2"
 
 # daily snapshots:
-#VERSION = VERSION + datetime.datetime.now().strftime(".%Y%m%d")
+VERSION = VERSION + datetime.datetime.now().strftime(".%Y%m%d")
 
 
 data_files = []
