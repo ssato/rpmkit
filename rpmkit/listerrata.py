8a3c6ee0bbd131a420013ad8e4bbdc74372023cf
Satoru SATOH <satoru.satoh@gmail.com>
2012-10-09 22:50:59 +0900

    make it work (but not tested for start and end options yet)

--- rpmkit/listerrata.py
+++ rpmkit/listerrata.py
@@ -17,7 +17,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-import rpmkit.swapi as SW
+from rpmkit.swapi import main as swmain
+from itertools import groupby
+
 
 import logging
 import optparse
@@ -29,14 +31,15 @@ import sys
 
 
 DATE_REG = re.compile(r'\d{4}-\d{2}-\d{2}\s+(?:\d{2}:\d{2}:\d{2})?')
-TIME_RESOLUTIONS = (_DAY, _MONTH, _YEAR) = (0, 1, 2)
 
 
-def __swapi(cmd_s):
+def __swapi(cmd):
     """
     swapi (SpaceWalk API library) wrapper.
+
+    :param cmd: command args :: [str]
     """
-    return SW.main(shlex.split(cmd_s))[0]
+    return swmain(cmd)[0]
 
 
 def validate_datetime(t, reg=DATE_REG):
@@ -48,7 +51,7 @@ def validate_datetime(t, reg=DATE_REG):
     return reg.match(t) is not None
 
 
-def list_errata(channel, start=None, end=None):
+def list_errata(channel, start=None, end=None, offline=False):
     """
     :param channel: Software channel label to list errata
     :param start: Start date
@@ -59,7 +62,7 @@ def list_errata(channel, start=None, end=None):
             "list_errata(): Invalid data for `start` parameter: '%s'" % start
 
     if end is not None:
-        assert start is None), \
+        assert start is None, \
             "list_errata(): `end` paramter requires `start` parameter is set"
         assert validate_datetime(end), \
             "list_errata(): Invalid data for `end` parameter: '%s'" % end
@@ -70,25 +73,91 @@ def list_errata(channel, start=None, end=None):
         "channel.software.listErrata"
     ]
 
-    return __swapi(' '.join(a for a in args if a is not None))
+    return __swapi([a for a in args if a is not None])
 
 
-def init_log(verbose):
-    """Initialize logging module
+_TIME_RESOLUTIONS = (_DAY, _MONTH, _YEAR) = (0, 1, 2)
+_TIME_RESOLUTION_S = dict(day=_DAY, month=_MONTH, year=_YEAR)
+
+
+def __keyfunc(resolution):
     """
-    level = logging.WARN  # default
+    >>> e = {"issue_date": "2012-09-03 13:00:00"}
+    >>> __keyfunc(_DAY)(e)
+    '2012-09-03'
+    >>> __keyfunc(_MONTH)(e)
+    '2012-09'
+    >>> __keyfunc(_YEAR)(e)
+    '2012'
+    """
+    def f(errata):
+        try:
+            return errata["issue_date"].split()[0].rsplit('-', resolution)[0]
+        except (IndexError, TypeError):
+            raise RuntimeError("errata was '%s'" % str(errata))
 
-    if verbose > 0:
-        level = logging.INFO
+    return f
 
-        if verbose > 1:
-            level = logging.DEBUG
 
-    logging.basicConfig(level=level)
+def div_errata_list_by_time_resolution(es, resolution=_DAY):
+    """
+    Divides given errata list (gotten by `list_errata` defined above) by given
+    time period `resolution`, returns list of list of errata.
+
+    :param es: Errata list
+    :param resolution: Time resolution
+    """
+    return [list(g) for k, g in groupby(es, key=__keyfunc(resolution))]
+
+
+def init_log(level):
+    lvl = [logging.DEBUG, logging.INFO, logging.WARN][level]
+    logging.basicConfig(format="[%(levelname)s] %(message)s", level=lvl)
+
+
+def option_parser():
+    p = optparse.OptionParser("%prog [OPTION ...] SW_CHANNEL_LABEL")
+
+    defaults = dict(
+        resolution="day", start=None, end=None, outdir=None, verbose=1,
+    )
+    p.set_defaults(**defaults)
+
+    p.add_option(
+        "-r", "--resolution", type="choice", choices=_TIME_RESOLUTION_S.keys(),
+        help="Specify time resolution to group errata list [%default]"
+    )
+    p.add_option("-s", "--start", help="Specify start date to list errata from")
+    p.add_option("-e", "--end", help="Specify end date to list errata to")
+
+    p.add_option("-D", "--debug", action="store_const", const=0,
+        dest="verbose", help="Debug mode"
+    )
+    p.add_option("-q", "--quiet", action="store_const", const=2,
+        dest="verbose", help="Quiet mode"
+    )
+    return p
 
 
 def main(argv=sys.argv):
-    pass
+    p = option_parser()
+    (options, args) = p.parse_args(argv[1:])
+
+    if not args:
+        p.print_help()
+        print >> sys.stderr, \
+            "\nTry `swapi channel.listSoftwareChannels` to get sw channels"
+        sys.exit(0)
+
+    init_log(options.verbose)
+
+    channel = args[0]
+    resolution = _TIME_RESOLUTION_S.get(options.resolution, "day")
+
+    es = list_errata(channel, options.start, options.end)
+    res = div_errata_list_by_time_resolution(es, resolution)
+
+    pprint.pprint(res)
 
 
 if __name__ == '__main__':
