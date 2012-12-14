e6a7898aadfbe1c42514eecfe52849de9b8cc7f5
Satoru SATOH <satoru.satoh@gmail.com>
2012-12-14 15:37:32 +0900

    0.2.10.9-rc1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ curdir = os.getcwd()
 sys.path.append(curdir)
 
 PACKAGE = "rpmkit"
-VERSION = "0.2.10.8"
+VERSION = "0.2.10.9"
 
 # daily snapshots:
 #VERSION = VERSION + datetime.datetime.now().strftime(".%Y%m%d")
