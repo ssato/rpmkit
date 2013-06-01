fe266bbadc1f3d5fa5ae06c1ea5304e79d0ca6de
Satoru SATOH <satoru.satoh@gmail.com>
2013-06-01 16:59:02 +0900

    follow rename from .make_hierarchical_nested_dicts_from_paths to make_tree_from_path in rpmkit.tree

--- rpmkit/tests/tree.py
+++ rpmkit/tests/tree.py
@@ -27,7 +27,7 @@ class Test_00_pure_functions(unittest.TestCase):
         self.assertEquals([n.name() for n in ret[0]], ["foo", "bar"])
         self.assertEquals([n.name() for n in ret[1]], ["foo", "baz", "aaa"])
 
-    def test_10_make_hierarchical_nested_dicts_from_paths(self):
+    def test_10_make_tree_from_path(self):
         paths = [['kbd-misc', 'kbd', 'dracut', 'dracut-kernel'],
                  ['kbd-misc', 'kbd', 'dracut', 'kexec-tools']]
         ref = dict(name="kbd-misc",
@@ -39,9 +39,58 @@ class Test_00_pure_functions(unittest.TestCase):
                                          dict(name="dracut-kernel"),
                                          dict(name="kexec-tools")])])])
 
-        result = TT.make_hierarchical_nested_dicts_from_paths(paths)
+        result = TT.make_tree_from_path(paths)
 
         self.assertEquals(result, ref)
 
+    def test_11_make_tree_from_path(self):
+        """
+        TODO: More complex cases.
+
+        paths = [['setup', 'basesystem'],
+                 ['setup', 'filesystem', 'basesystem'],
+                 ['setup', 'filesystem', 'dracut', 'dracut-kernel'],
+                 ['setup', 'filesystem', 'dracut', 'kexec-tools'],
+                 ['setup', 'filesystem', 'dracut', 'postfix', 'cronie',
+                  'sysstat'],
+                 ['setup', 'filesystem', 'dracut', 'postfix', 'cronie',
+                  'cronie-anacron', 'crontabs', 'shadow-utils', 'mlocate']]
+
+        part = dict(name="cronie",
+                    children=[
+                        dict(name="sysstat"),
+                        dict(name="cronie-anacron",
+                             children=[
+                                  dict(name="crontabs",
+                                       children=[
+                                            dict(name="shadow-utils",
+                                                 children=[
+                                                      dict(name="mlocate"),
+                                                 ])
+                                       ])
+                             ])
+                    ])
+
+        ref = dict(name="setup",
+                   children=[
+                       dict(name="basesystem"),
+                       dict(name="filesystem",
+                            children=[
+                                dict(name="basesystem"),
+                                dict(name="dracut",
+                                     children=[
+                                         dict(name="dracut-kernel"),
+                                         dict(name="kexec-tools"),
+                                         dict(name="postfix",
+                                              children=[part])
+                                     ])
+                            ])
+                   ])
+
+        result = TT.make_tree_from_path(paths)
+
+        self.assertEquals(result, ref)
+        """
+        pass
 
 # vim:sw=4 ts=4 et:
