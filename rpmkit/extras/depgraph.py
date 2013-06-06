b03fa0154f39b42960ca5f39cc8785659938880e
Satoru SATOH <satoru.satoh@gmail.com>
2013-06-06 11:54:27 +0900

    add make_rpm_dependencies_dag in rpmkit.extras.depgraph to make a DAG graph (networkx.DiGraph) from RPM dependency graph (directed non-aacyclic graph)

--- rpmkit/extras/depgraph.py
+++ rpmkit/extras/depgraph.py
@@ -18,6 +18,7 @@
 from rpmkit.memoize import memoize
 
 import rpmkit.rpmutils as RU
+import rpmkit.utils as U
 import networkx as NX
 
 
@@ -71,8 +72,96 @@ def list_rpms_having_cyclic_dependencies(root, reversed=True, rreqs=None):
 
     :return: [[rpm_name]]; Each list represents strongly connected RPMs.
     """
-
     G = make_dependency_graph_with_nx(root, reversed, rreqs)
     return NX.simple_cycles(G)
+
+
+def _degenerate_node(nodes, sep='|'):
+    """
+    :param nodes: List of strongly connected nodes :: [str]
+    :return: Degenerated node :: str
+    """
+    return sep.join(nodes)
+
+
+def _degenerate_nodes(G, nodes, reqs, rreqs, sep='|'):
+    """
+    For each node, remove edges from/to that node and the node from the graph
+    ``G`` and then add newly 'degenerated' node and relevant edges again.
+
+    :param G: Dependency graph of nodes
+    :param nodes: Node (name) list
+    """
+    for node in nodes:
+        G.remove_edges_from([(node, p) for p in rreqs.get(node, [])])
+        G.remove_edges_from([(r, node) for r in reqs.get(node, [])])
+
+    G.remove_nodes_from(nodes)
+
+    dnode = _degenerate_node(nodes)
+    G.add_node(dnode)
+
+    dnode_rreqs = U.uconcat([p for p in rreqs.get(node, []) if p not in nodes]
+                            for node in nodes)
+    dnode_reqs = U.uconcat([r for r in reqs.get(node, []) if r not in nodes]
+                           for node in nodes)
+
+    if dnode_rreqs:
+        G.add_edges_from([(dnode, p) for p in dnode_rreqs])
+
+    if dnode_reqs:
+        G.add_edges_from([(r, dnode) for r in dnode_reqs])
+
+    return G
+
+
+def make_rpm_dependencies_dag(root, reqs=None, rreqs=None):
+    """
+    Make direct acyclic graph from RPM dependencies.
+
+    see also:
+
+    * http://en.wikipedia.org/wiki/Directed_acyclic_graph
+    * http://en.wikipedia.org/wiki/Strongly_connected_component
+
+    :param root: RPM Database root dir
+    :param rreqs: A dict represents RPM dependencies;
+        {x: [package_requires_x]} or {x: [package_required_by_x]}.
+
+    :return: networkx.DiGraph instance represents the dag of rpm deps.
+    """
+    if rreqs is None:
+        rreqs = RU.make_reversed_requires_dict(root)
+
+    if reqs is None:
+        reqs = RU.make_requires_dict(root)
+
+    G = make_dependency_graph_with_nx(root, rreqs=rreqs)
+
+    # Remove edges of self cyclic nodes:
+    G.remove_edges_from(G.selfloop_edges())
+
+    # Degenerate strongly connected components:
+    for scc in NX.strongly_connected_components(G):
+        scc = sorted(U.uniq(scc))
+
+        if len(scc) == 1:  # Ignore sccs of which length is 1.
+            continue
+
+        G = _degenerate_nodes(G, scc, reqs, rreqs, '|')
+
+    # Degenerate cyclic nodes:
+    for cns in NX.simple_cycles(G):
+        cns = sorted(U.uniq(cns))
+
+        # Should not happen as selc cyclic nodes were removed in advance.
+        assert len(cns) != 1, "Self cyclic node: " + cns[0]
+
+        G = _degenerate_nodes(G, cns, reqs, rreqs, ',')
+
+    assert NX.is_directed_acyclic_graph(G), \
+           "I'm still missing something to make it dag..."
+
+    return G
 
 # vim:sw=4:ts=4:et:
