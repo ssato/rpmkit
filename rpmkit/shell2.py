b0857cca4741e335f07d42ae34de5a6e49cf1e06
Satoru SATOH <satoru.satoh@gmail.com>
2013-07-15 11:20:33 +0900

    Fixes around timeout and error handling

--- rpmkit/shell2.py
+++ rpmkit/shell2.py
@@ -24,9 +24,6 @@ import os.path
 import subprocess
 
 
-_CONNECT_TIMEOUT = 10    # 10 [sec]
-
-
 def _is_local(fqdn_or_hostname):
     """
     >>> _is_local("localhost")
@@ -48,31 +45,71 @@ def _validate_timeout(timeout):
     >>> _validate_timeout(10)
     >>> _validate_timeout(0)
     >>> _validate_timeout(None)
-    >>> _validate_timeout(-1)
-    AssertionError: Invalid timeout: -1
+    >>> try:
+    ...     _validate_timeout(-1)
+    ... except AssertionError:
+    ...     pass
     """
     assert timeout is None or int(timeout) >= 0, \
         "Invalid timeout: " + str(timeout)
 
 
-def run_async(cmd, user=None, host="localhost", workdir=os.curdir,
-              timeout=None, connect_timeout=_CONNECT_TIMEOUT,
-              stop_on_error=False, **kwargs):
+class Timeout(object):
     """
-    Run command.
+
+    >>> timeo = Timeout(10, 10)
+    >>> timeo.exec_timeout() == 10
+    True
+    >>> timeo.set_exec_timeout(None)
+    >>> timeo.exec_timeout() != 10
+    True
+    >>> timeo.exec_timeout() is None
+    True
+    """
+
+    def __init__(self, exec_timeout=None, conn_timeout=None):
+        _validate_timeout(exec_timeout)
+        _validate_timeout(conn_timeout)
+
+        self._exec_to = exec_timeout
+        self._conn_to = conn_timeout
+
+    def exec_timeout(self):
+        return self._exec_to
+
+    def conn_timeout(self):
+        return self._conn_to
+
+    def set_exec_timeout(self, val):
+        _validate_timeout(val)
+        self._exec_to = val
+
+    def set_conn_timeout(self, val):
+        _validate_timeout(val)
+        self._conn_to = val
+
+    def validate(self):
+        _validate_timeout(self._exec_to)
+        _validate_timeout(self._conn_to)
+
+
+_TIMEOUT = Timeout(None, 10)
+
+
+def run_async(cmd, user=None, host="localhost", workdir=os.curdir,
+              timeout=_TIMEOUT, **kwargs):
+    """
+    Run command asyncronously.
 
     :param cmd: Command string
     :param user: User to run command
     :param host: Host to run command
     :param workdir: Working directory in which command runs
-    :param timeout: Command execution timeout in seconds
-    :param connect_timeout: Connect timeout in seconds
-    :param stop_on_error: Do not catch exceptions of errors if true
+    :param timeout: Timeout object
 
     :return: greenlet instance
     """
-    _validate_timeout(timeout)
-    _validate_timeout(connect_timeout)
+    timeout.validate()
 
     if _is_local(host):
         if "~" in workdir:
@@ -81,10 +118,10 @@ def run_async(cmd, user=None, host="localhost", workdir=os.curdir,
         if user is None:
             user = E.get_username()
 
-        if connect_timeout is None:
+        if timeout.conn_timeout() is None:
             toopt = ""
         else:
-            toopt = "-o ConnectTimeout=%d" % connect_timeout
+            toopt = "-o ConnectTimeout=%d" % timeout.conn_timeout()
 
         cmd = "ssh %s %s@%s 'cd %s && %s'" % (toopt, user, host, workdir, cmd)
         logging.debug("Remote host. Rewrote cmd to " + cmd)
@@ -95,8 +132,7 @@ def run_async(cmd, user=None, host="localhost", workdir=os.curdir,
 
 
 def run(cmd, user=None, host="localhost", workdir=os.curdir,
-        timeout=None, connect_timeout=_CONNECT_TIMEOUT,
-        stop_on_error=False, **kwargs):
+        timeout=_TIMEOUT, stop_on_error=False, **kwargs):
     """
     Run command.
 
@@ -104,22 +140,28 @@ def run(cmd, user=None, host="localhost", workdir=os.curdir,
     :param user: User to run command
     :param host: Host to run command
     :param workdir: Working directory in which command runs
-    :param timeout: Command execution timeout in seconds
-    :param connect_timeout: Connect timeout in seconds
-    :param stop_on_error: Do not catch exceptions of errors if true
+    :param timeout: Timeout object
+    :param stop_on_error: Stop and raise exception if any error occurs
 
-    :return: True if job was sucessful else False
+    :return: True if job was sucessful else False or RuntimeError exception
+        raised if stop_on_error is True
     """
-    job = run_async(cmd, user, host, workdir, timeout, connect_timeout,
-                    stop_on_error, **kwargs)
-    job.join(timeout)
+    job = run_async(cmd, user, host, workdir, timeout)
+    job.join(timeout.exec_timeout())
 
-    return job.successful()  # TODO: ... or job.exception
+    if not job.successful():
+        if stop_on_error:
+            raise RuntimeError("Failed: " + cmd)
+
+        logging.warn("Failed: " + cmd)
+        return False
+
+    return True
 
 
 def prun_async(list_of_args):
     """
-    Run commands. 
+    Run commands in parallel asyncronously.
 
     :param list_of_args: List of arguments (:: [([arg], dict(kwarg=...))]) for
         each job will be passed to ``run_async`` function.
@@ -131,7 +173,7 @@ def prun_async(list_of_args):
 
 def prun(list_of_args):
     """
-    Run commands. 
+    Run commands in parallel.
 
     :param list_of_args: List of arguments (:: [([arg], dict(kwarg=...))]) for
         each job will be passed to ``run_async`` function.
@@ -139,6 +181,5 @@ def prun(list_of_args):
     :return: List of status of each job :: [bool]
     """
     return [run(*args, **kwargs) for args, kwargs in list_of_args]
-
 
 # vim:sw=4:ts=4:et:
