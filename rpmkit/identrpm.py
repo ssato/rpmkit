54ffb6486926438361d26d4266630c8047491165
Satoru SATOH <satoru.satoh@gmail.com>
2014-01-17 18:21:04 +0900

    rpmkit.identrpm: cleanup the header

--- rpmkit/identrpm.py
+++ rpmkit/identrpm.py
@@ -1,21 +1,9 @@
 #
-# Identify given RPMs with using RHN API.
+# Identify given RPMs list with using RHN package search APIs.
 #
-# Copyright (C) 2011, 2012 Red Hat, Inc.
+# Copyright (C) 2011 - 2014 Red Hat, Inc.
 # Red Hat Author(s): Satoru SATOH <ssato@redhat.com>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# License: GPLv3+
 #
 import rpmkit.swapi as SW
 import rpmkit.rpmutils as RU
