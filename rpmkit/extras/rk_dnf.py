5e505037bd7829fffd369db915ae7bfd2ef6f6be
Satoru SATOH <satoru.satoh@gmail.com>
2014-08-11 22:32:56 +0900

    override dnf.{base,repo,...} to load updateinfo

--- rpmkit/extras/rk_dnf.py
+++ rpmkit/extras/rk_dnf.py
@@ -6,11 +6,18 @@
 """Some utility routines utilize DNF/Hawkey.
 """
 from __future__ import print_function
+from dnf.i18n import _, ucd
+from dnf.repo import _subst2tuples
 
+import dnf.base
+# import dnf.conf.read  # It exists in git but not available in
+                        # dnf-0.5.4-2.fc20.noarch
 import dnf.cli.cli
 import dnf.exceptions
+import dnf.repo
 import dnf.subject
 import dnf.transaction
+import functools
 import glob
 import librepo
 import logging
@@ -19,14 +26,156 @@ import shutil
 import tempfile
 
 
+# Inherit and extend some classes in dnf to support updateinfo.
+#
+# This is required to get support updateinfo in dnf until patches in the
+# following pull request is merged:
+#   https://github.com/akozumpl/dnf/pull/143/files
+#
+class _Handle(dnf.repo._Handle):
+
+    def __init__(self, gpgcheck, max_mirror_tries):
+        super(_Handle, self).__init__(gpgcheck, max_mirror_tries)
+        self.yumdlist.append("updateinfo")
+
+
+class Metadata(dnf.repo.Metadata):
+
+    @property
+    def updateinfo_fn(self):
+        return self.repo_dct.get('updateinfo')
+
+
+class Repo(dnf.repo.Repo):
+
+    def _handle_load(self, handle):
+        """
+        Extended version of dnf.repo.Repo._handle_load to make and return an
+        instance of the above :class:`Metadata` instead of :class:`.
+
+        (Actually, I just copied the original w/o any modifications.)
+        """
+        if handle.progresscb:
+            self._md_pload.start(self.name)
+        result = handle.perform()
+        if handle.progresscb:
+            self._md_pload.end()
+        return Metadata(result, handle)
+
+    def _handle_new_local(self, destdir):
+        """Likewise."""
+        return _Handle.new_local(self.substitutions, self.repo_gpgcheck,
+                                 self.max_mirror_tries, destdir)
+
+    def _handle_new_remote(self, destdir, mirror_setup=True):
+        """Likewise."""
+        h = _Handle(self.repo_gpgcheck, self.max_mirror_tries)
+        h.varsub = _subst2tuples(self.substitutions)
+        h.destdir = destdir
+        self._set_ip_resolve(h)
+
+        # setup mirror URLs
+        mirrorlist = self.metalink or self.mirrorlist
+        if mirrorlist:
+            if mirror_setup:
+                h.setopt(librepo.LRO_MIRRORLIST, mirrorlist)
+                h.setopt(librepo.LRO_FASTESTMIRROR, self.fastestmirror)
+                h.setopt(librepo.LRO_FASTESTMIRRORCACHE,
+                         os.path.join(self.basecachedir, 'fastestmirror.cache'))
+            else:
+                # use already resolved mirror list
+                h.setopt(librepo.LRO_URLS, self.metadata.mirrors)
+        elif self.baseurl:
+            h.setopt(librepo.LRO_URLS, self.baseurl)
+        else:
+            msg = 'Cannot find a valid baseurl for repo: %s' % self.id
+            raise dnf.exceptions.RepoError(msg)
+
+        # setup download progress
+        h.progresscb = self._md_pload._progress_cb
+        self._md_pload.fm_running = False
+        h.fastestmirrorcb = self._md_pload._fastestmirror_cb
+
+        # apply repo options
+        h.maxspeed = self.throttle if type(self.throttle) is int \
+                     else int(self.bandwidth * self.throttle)
+        h.proxy = self.proxy
+        h.sslverifypeer = h.sslverifyhost = self.sslverify
+
+        return h
+
+    @property
+    def updateinfo_fn(self):
+        """Added property."""
+        return self.metadata.updateinfo_fn
+
+
+class BaseCli(dnf.cli.cli.BaseCli):
+    """
+    Extend to override some methods in :class:`dnf.base.Base`, base class
+    of :class:`dnf.cli.cli.BaseCli`, to utilize some classes above instead
+    of original ones.
+    """
+
+    def _add_repo_to_sack(self, name):
+        repo = self.repos[name]
+        try:
+            repo.load()
+        except dnf.exceptions.RepoError as e:
+            if repo.skip_if_unavailable is False:
+                raise
+            self.logger.warning(_("%s, disabling."), e)
+            repo.disable()
+            return
+        hrepo = repo.hawkey_repo
+        hrepo.repomd_fn = repo.repomd_fn
+        hrepo.primary_fn = repo.primary_fn
+        hrepo.filelists_fn = repo.filelists_fn
+        hrepo.cost = repo.cost
+        if repo.presto_fn:
+            hrepo.presto_fn = repo.presto_fn
+        else:
+            self.logger.debug("not found deltainfo for: %s" % repo.name)
+
+        # Added to load updateinfo metadata.
+        if repo.updateinfo_fn:
+            hrepo.updateinfo_fn = repo.updateinfo_fn
+        else:
+            self.logger.debug("not found updateinfo for: %s" % repo.name)
+        repo.hawkey_repo = hrepo
+        self._sack.load_yum_repo(hrepo, build_cache=True, load_filelists=True,
+                                 load_presto=repo.deltarpm,
+                                 load_updateinfo=bool(repo.updateinfo_fn))
+
+    def readRepoConfig(self, parser, section):
+        repo = Repo(section, self.conf.cachedir)  # Changed: s/dnf.repo.Repo/Repo/
+        try:
+            repo.populate(parser, section, self.conf)
+        except ValueError as e:
+            msg = _('Repository %r: Error parsing config: %s' % (section, e))
+            raise dnf.exceptions.ConfigError(msg)
+
+        # Ensure that the repo name is set
+        if not repo.name:
+            repo.name = section
+            self.logger.error(_('Repository %r is missing name in configuration, '
+                    'using id') % section)
+        repo.name = ucd(repo.name)
+
+        repo.substitutions.update(self.conf.substitutions)
+        repo.cfg = parser
+
+        return repo
+
+
 def base_create(root):
     """
     Create and initialize dnf.base object.
 
     :param root: RPM DB root dir
-    :return: A dnf.cli.cli.BaseCli (dnf.base) object
+    :return: A Base object (see above)
     """
-    base = dnf.cli.cli.BaseCli()
+    base = BaseCli()
 
     if root != '/':
         base.conf.installroot = base.conf.cachedir = os.path.abspath(root)
@@ -96,9 +245,12 @@ def download_updateinfo_xml(repo):
     h.perform()  # FIXME: Handle exceptions.
 
     updixml = glob.glob(os.path.join(tmpdir, 'repodata', '*updateinfo*'))[0]
-    shutil.move(updixml, os.path.join(repo.cachedir, "repodata",
-                                      os.path.basename(updixml)))
+    updixml_new = os.path.join(repo.cachedir, "repodata",
+                               os.path.basename(updixml))
+    shutil.move(updixml, updixml_new)
     shutil.rmtree(tmpdir)
+
+    return updixml_new
 
 
 def compute_removed(pkgspecs, root, excludes=[]):
@@ -138,7 +290,7 @@ def compute_removed(pkgspecs, root, excludes=[]):
     return (sorted(set(excludes)), sorted(set(removes)))
 
 
-def base_fill_sack(root, repos=[], updateinfo=False, setup_callbacks=False):
+def base_fill_sack(root, repos=[], setup_callbacks=False):
     """
     :param root: RPM DB root dir (relative or absolute)
     :param repos: A list of repos to enable or []. [] means that all available
@@ -174,12 +326,6 @@ def base_fill_sack(root, repos=[], updateinfo=False, setup_callbacks=False):
     # see :method:`run` in :class:`dnf.cli.cli.Cli`.
     base.fill_sack(load_system_repo='auto',
                    load_available_repos=base.repos.enabled())
-
-    if updateinfo:
-        for rid, repo in base.repos.iteritems():
-            if repo.enabled:
-                download_updateinfo_xml(repo)
-
     return base
 
 
@@ -192,7 +338,7 @@ def compute_updates(root, repos=[], setup_callbacks=False):
 
     :return: A pair of a list of packages
     """
-    base = base_fill_sack(root, repos, True, setup_callbacks)
+    base = base_fill_sack(root, repos, setup_callbacks)
 
     # see :method:`run` in :class:`dnf.cli.commands.CheckUpdateCommand`.
     ypl = base.returnPkgLists(["updates"])
