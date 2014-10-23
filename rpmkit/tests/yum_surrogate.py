788204b7eaaa42bcbeff586908aa644c1e7b3e69
Satoru SATOH <satoru.satoh@gmail.com>
2014-10-23 18:01:39 +0900

    update the test cases of rpmkit.yum_surrogate.*

--- rpmkit/tests/yum_surrogate.py
+++ rpmkit/tests/yum_surrogate.py
@@ -1,111 +1,40 @@
 #
-# Copyright (C) 2013 Red Hat, Inc.
+# Copyright (C) 2014 Red Hat, Inc.
 # Red Hat Author(s): Satoru SATOH <ssato at redhat.com>
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# This software is licensed to you under the GNU General Public License,
+# version 3 (GPLv3). There is NO WARRANTY for this software, express or
+# implied, including the implied warranties of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. You should have received a copy of GPLv3 along with this
+# software; if not, see http://www.gnu.org/licenses/gpl.html
 #
 import rpmkit.yum_surrogate as TT
+import rpmkit.updateinfo.utils as RUU
 import rpmkit.tests.common as C
 
-import bsddb
-import os
 import os.path
+import os
+import shutil
 import unittest
 
 
-class Test_04_is_bsd_hashdb(unittest.TestCase):
+if RUU.is_rhel_or_fedora():
+    class Test_10_effectful_functions(unittest.TestCase):
 
-    def setUp(self):
-        self.workdir = C.setup_workdir()
+        def setUp(self):
+            self.workdir = C.setup_workdir()
 
-    def tearDown(self):
-        C.cleanup_workdir(self.workdir)
+            rpmdbdir = os.path.join(self.workdir, RUU.RPMDB_SUBDIR)
+            os.makedirs(rpmdbdir)
 
-    def test_00_is_bsd_hashdb__True(self):
-        path = os.path.join(self.workdir, "bsdhashdb")
-        bsddb.hashopen(path)
+            for dbn in RUU._RPM_DB_FILENAMES:
+                shutil.copy(os.path.join('/', RUU.RPMDB_SUBDIR, dbn), rpmdbdir)
 
-        self.assertTrue(TT.is_bsd_hashdb(path))
+        def tearDown(self):
+            C.cleanup_workdir(self.workdir)
 
-    def test_10_is_bsd_hashdb__False(self):
-        path = os.path.join(self.workdir, "bsdhashdb")
-        open(path, 'w').write('\n')
-
-        self.assertFalse(TT.is_bsd_hashdb(path))
-
-
-class Test_05_find_Packages_rpmdb(unittest.TestCase):
-
-    def setUp(self):
-        self.workdir = C.setup_workdir()
-
-    def tearDown(self):
-        C.cleanup_workdir(self.workdir)
-
-    def test_00_find_Packages_rpmdb__found(self):
-        path = os.path.join(self.workdir, "var", "lib", "rpm", "Packages")
-        os.makedirs(os.path.dirname(path))
-
-        bsddb.hashopen(path)
-
-        self.assertEquals(TT.find_Packages_rpmdb(self.workdir), path)
-
-
-def setup_Packages(topdir, subdir=TT._RPMDB_SUBDIR):
-    path = os.path.join(topdir, subdir, "Packages")
-    os.makedirs(os.path.dirname(path))
-
-    bsddb.hashopen(path)
-
-    return path
-
-
-class Test_06_setup_root(unittest.TestCase):
-
-    def setUp(self):
-        self.workdir = C.setup_workdir()
-
-    def tearDown(self):
-        C.cleanup_workdir(self.workdir)
-
-    def test_00_setup_root__exact_path(self):
-        ppath = setup_Packages(self.workdir)
-        self.assertEquals(TT.setup_root(ppath, refer_other_rpmdb=False),
-                          os.path.abspath(self.workdir))
-
-    def test_10_setup_root__find(self):
-        ppath = setup_Packages(self.workdir)
-        self.assertEquals(TT.setup_root(os.path.dirname(ppath),
-                                        refer_other_rpmdb=False),
-                          os.path.abspath(self.workdir))
-
-        self.assertEquals(TT.setup_root(self.workdir, refer_other_rpmdb=False),
-                          os.path.abspath(self.workdir))
-
-    def test_20_setup_root__w_pivot_root(self):
-        ppath = setup_Packages(self.workdir)
-        root = os.path.join(self.workdir, "pivot_root")
-
-        self.assertEquals(TT.setup_root(ppath, root, refer_other_rpmdb=False),
-                          root)
-
-    def test_30_setup_root__w_root(self):
-        ppath = setup_Packages(self.workdir)
-        root = self.workdir
-
-        self.assertEquals(TT.setup_root(ppath, root, refer_other_rpmdb=False),
-                          root)
-
+        def test_10_run_yum_cmd(self):
+            for c in ("list-sec", "list installed"):
+                TT.run_yum_cmd(self.workdir, c)
 
 # vim:sw=4:ts=4:et:
