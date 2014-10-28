32745e2f3b937fdc96b2d44faaef120439299fcc
Satoru SATOH <satoru.satoh@gmail.com>
2014-10-28 15:08:55 +0900

    make a class of some methods implements interfaces in rpmkit.updateinfo.base.Base instead of functions, in rpmkit.updateinfo.dnfbase

--- rpmkit/updateinfo/dnfbase.py
+++ rpmkit/updateinfo/dnfbase.py
@@ -5,6 +5,8 @@
 #
 """Some utility routines utilize DNF/Hawkey.
 """
+import rpmkit.updateinfo.base
+
 import dnf.cli.cli
 import dnf
 import logging
@@ -66,101 +68,123 @@ def _activate_repos(base, repos=[], disabled_repos=['*']):
     _toggle_repos(base, repos, _REPO_ENABLE)
 
 
-def create(root, repos=[], disabled_repos=['*'], cli=True,
-           pkgnarrow="installed"):
-    """
-    Create and initialize dnf.Base object.
+class Base(rpmkit.updateinfo.base.Base):
 
-    :param root: RPM DB root dir
-    :param repos: A list of repos to enable
-    :param disabled_repos: A list of repos to disable
-    :param cli: Return an instance of dnf.cli.cli.BaseCli instead of dnf.Base
-        if True
-    :return: A dnf.cli.cli.BaseCli (dnf.Base) object
+    def __init__(self, root='/', repos=[], disabled_repos=['*'],
+                 workdir=None, cli=True, cacheonly=False, **kwargs):
+        """
+        Create and initialize dnf.Base or dnf.cli.cli.BaseCli object.
 
-    see also: :function:`dnf.automatic.main.main`
+        :param root: RPM DB root dir
+        :param repos: A list of repos to enable
+        :param disabled_repos: A list of repos to disable
+        :param workdir: Working dir to save logs and results
+        :param cli: Which objects to instanciate? dnf.cli.cli.BaseCli
+            if True or dnf.Base
 
-    >>> if os.path.exists("/etc/redhat-release"):
-    ...     base = create('/')
-    ...     assert isinstance(base, dnf.Base)
-    ...
-    ...     base = create('/', pkgnarrow='all')
-    ...     assert isinstance(base, dnf.Base)
-    """
-    base = (dnf.cli.cli.BaseCli if cli else dnf.Base)()
+        see also: :function:`dnf.automatic.main.main`
 
-    if root != '/':
-        root = os.path.abspath(root)
-        base.conf.installroot = base.conf.cachedir = os.path.abspath(root)
+        >>> if os.path.exists("/etc/redhat-release"):
+        ...     base = Base(cli=False)
+        ...     assert isinstance(base.base, dnf.Base)
+        ...
+        ...     base = Base(cli=True)
+        ...     assert isinstance(base.base, dnf.cli.cli.BaseCli)
+        """
+        super(Base, self).__init__(root, repos, disabled_repos, **kwargs)
+        self.base = (dnf.cli.cli.BaseCli if cli else dnf.Base)()
 
-    if pkgnarrow == "installed":  # Make it just loading RPM DB.
-        base.fill_sack(load_system_repo='auto', load_available_repos=False)
-    else:
-        base.read_all_repos()
-        _activate_repos(base, repos, disabled_repos)
+        if self.root != '/':
+            self.base.conf.installroot = self.base.conf.cachedir = self.root
 
-    return base
+        self.cacheonly = cacheonly
 
+        # TODO: Ugly
+        self.ready_list_install = False
+        self.ready_list_x = False
 
-def load_repos(base, setup_callbacks=False):
-    """
-    Activate and load metadata of base's repos, etc.
+    def prepare(self, pkgnarrow="installed"):
+        if pkgnarrow == "installed":  # Make it just loading RPM DB.
+            if self.ready_list_install:
+                return
 
-    :param base: dnf.Base instance
-    :param setup_callbacks: Setup callbacks and progress bar displayed if True
+            self.base.fill_sack(load_system_repo='auto',
+                                load_available_repos=False)
+            self.ready_list_install = True
+        else:
+            if self.ready_list_x:
+                return
 
-    see also: :function:`dnf.automatic.main.main`
-    """
-    assert isinstance(base, dnf.cli.cli.BaseCli), \
-        "Wrong base object: %s" % str(base)
+            self.base.read_all_repos()
+            _activate_repos(self.base, self.repos, self.disabled_repos)
 
-    # see :method:`configure` in :class:`dnf.cli.cli.Cli`.
-    base.activate_persistor()
+            for repo in self.base.repos.iter_enabled():
+                repo.skip_if_unavailable = True
 
-    # see :method:`_configure_repos` in :class:`dnf.cli.cli.Cli`.
-    if setup_callbacks:
-        (bar, base.ds_callback) = base.output.setup_progress_callbacks()
-        base.repos.all.set_progress_bar(bar)
-        # pylint: disable=no-member
-        base.repos.all.confirm_func = base.output._cli_confirm_gpg_key_import
-        # pylint: enable=no-member
+                if self.cacheonly:
+                    repo.md_only_cached = True
 
-    # It will take some time to get metadata from remote repos.
-    # see :method:`run` in :class:`dnf.cli.cli.Cli`.
-    base.fill_sack(load_system_repo="auto")
+            self.ready_list_x = True
 
+    def load_repos(self, setup_callbacks=False):
+        """
+        Activate and load metadata of base's repos, etc.
 
-# functions to mimic some yum commands.
-def list_installed(root):
-    """
-    :param root: RPM DB root dir (relative or absolute)
+        :param setup_callbacks: Setup callbacks and progress bar displayed
+            if True
 
-    >>> if os.path.exists("/etc/redhat-release"):
-    ...     ipkgs = list_installed('/')
-    ...     assert len(ipkgs) > 0
-    """
-    base = create(root, cli=False)
-    return base.sack.query().installed().run()
+        see also: :function:`dnf.automatic.main.main`
+        """
+        base = self.base
 
+        # see :method:`configure` in :class:`dnf.cli.cli.Cli`.
+        base.activate_persistor()
 
-def list_updates(root, repos=[], disabled_repos=['*'], obsoletes=True):
-    """
-    :param root: RPM DB root dir (relative or absolute)
-    :param repos: List of Yum repos to enable
-    :param disabled_repos: List of Yum repos to disable
-    :param obsoletes: Include obsoletes in updates list if True
+        # see :method:`_configure_repos` in :class:`dnf.cli.cli.Cli`.
+        if setup_callbacks:
+            (bar, base.ds_callback) = base.output.setup_progress_callbacks()
+            base.repos.all.set_progress_bar(bar)
+            # pylint: disable=no-member
+            base.repos.all.confirm_func = \
+                base.output._cli_confirm_gpg_key_import
+            # pylint: enable=no-member
 
-    >>> if os.path.exists("/etc/redhat-release"):
-    ...     xs = list_updates('/', repos=['*'], disabled_repos=[])
-    ...     assert isinstance(xs, list), str(xs)
-    """
-    base = create(root, pkgnarrow="all")
-    load_repos(base)
+        # It will take some time to get metadata from remote repos.
+        # see :method:`run` in :class:`dnf.cli.cli.Cli`.
+        base.fill_sack(load_system_repo="auto")
 
-    # see :method:`check_updates` in :class:`dnf.cli.cli.BaseCli`
-    ups = base.check_updates(print_=False).run()
-    # del base.ts  # Needed to release RPM DB session ?
+    def list_installed(self):
+        """
+        >>> if os.path.exists("/etc/redhat-release"):
+        ...     base = Base(cacheonly=True)
+        ...     ipkgs = base.list_installed()
+        ...     assert len(ipkgs) > 0
+        """
+        self.prepare()
+        return self.base.sack.query().installed().run()
 
-    return ups
+    def list_updates(self, obsoletes=True):
+        """
+        :param obsoletes: Include obsoletes in updates list if True
+
+        >>> if os.path.exists("/etc/redhat-release"):
+        ...     base = Base(repos=['*'], disabled_repos=[], cacheonly=True)
+        ...     xs = base.list_updates()
+        ...     assert isinstance(xs, list), str(xs)
+        """
+        self.prepare("all")
+        self.load_repos()
+
+        # see :method:`check_updates` in :class:`dnf.cli.cli.BaseCli`
+        ups = self.base.check_updates(print_=False).run()
+        # del self.base.ts  # Needed to release RPM DB session ?
+
+        return ups
+
+    def list_errata(self):
+        """
+        TBD.
+        """
+        raise NotImplementedError("dnfbase.Base.list_updates")
 
 # vim:sw=4:ts=4:et:
