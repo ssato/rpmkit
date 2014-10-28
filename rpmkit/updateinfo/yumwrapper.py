b5e39425c348f1fc7287bcf66f5d126e8c19d128
Satoru SATOH <satoru.satoh@gmail.com>
2014-10-28 12:49:35 +0900

    just update the copyright header (simplified a lot)

--- rpmkit/updateinfo/yumwrapper.py
+++ rpmkit/updateinfo/yumwrapper.py
@@ -1,12 +1,7 @@
 #
 # Copyright (C) 2014 Red Hat, Inc.
 # Red Hat Author(s): Satoru SATOH <ssato@redhat.com>
-#
-# This software is licensed to you under the GNU General Public License,
-# version 3 (GPLv3). There is NO WARRANTY for this software, express or
-# implied, including the implied warranties of MERCHANTABILITY or FITNESS FOR A
-# PARTICULAR PURPOSE. You should have received a copy of GPLv3 along with this
-# software; if not, see http://www.gnu.org/licenses/gpl.html
+# License: GPLv3+
 #
 import rpmkit.updateinfo.base
 import rpmkit.updateinfo.subproc
